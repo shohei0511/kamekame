5f62f76d7dde9b5accf809a141dba2f057db3cb0
shohei0511 <81957696+shohei0511@users.noreply.github.com>
2021-04-26 11:58:39 +0900

    Create thanks.php

---
+++ thanks.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>お問い合わせフォーム</title>
+<link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+<div><h1>kamekame diving shop</h1></div>
+    <div>
+        <div>
+        <h1>Reservation completed</h1>
+        <p>Looking forward to the day you can see.</p>
+        
+    </div>
+</div>
+</body>
+</html>
