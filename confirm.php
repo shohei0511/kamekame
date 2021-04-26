48b3595f2d8d6c2872edacd9da26664bf55f3efe
shohei0511 <81957696+shohei0511@users.noreply.github.com>
2021-04-26 11:58:17 +0900

    Create confirm.php

---
+++ confirm.php
@@ -0,0 +1,119 @@
+<?php 
+    // フォームのボタンが押されたら
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // フォームから送信されたデータを各変数に格納
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $tel = $_POST["tel"];
+        $sex = $_POST["sex"];
+        $item = $_POST["item"];
+    }
+
+    // 送信ボタンが押されたら
+    if (isset($_POST["submit"])) {
+        // 送信ボタンが押された時に動作する処理をここに記述する
+            
+        // 日本語をメールで送る場合のおまじない
+            mb_language("ja");
+        mb_internal_encoding("UTF-8");
+        
+        //mb_send_mail("kanda.it.school.trial@gmail.com", "メール送信テスト", "メール本文");
+
+            // 件名を変数subjectに格納
+            $subject = "［自動送信］お問い合わせ内容の確認";
+
+            // メール本文を変数bodyに格納
+        $body = <<< EOM
+{$name}　様
+
+Is there a mistake in your reservation?
+===================================================
+【 お名前 】 
+{$name}
+
+【 ふりがな 】 
+{$furigana}
+
+【 メール 】 
+{$email}
+
+【 電話番号 】 
+{$tel}
+
+【 性別 】 
+{$sex}
+
+【 項目 】 
+{$item}
+
+【 内容 】 
+{$content}
+===================================================
+
+内容を確認のうえ、回答させて頂きます。
+しばらくお待ちください。
+EOM;
+        
+        // 送信元のメールアドレスを変数fromEmailに格納
+        $fromEmail = "contact@dream-php-seminar.com";
+
+        // 送信元の名前を変数fromNameに格納
+        $fromName = "お問い合わせテスト";
+
+        // ヘッダ情報を変数headerに格納する      
+        $header = "From: " .mb_encode_mimeheader($fromName) ."<{$fromEmail}>";
+
+        // メール送信を行う
+        mb_send_mail($email, $subject, $body, $header);
+
+        // サンクスページに画面遷移させる
+        header("Location:/thanks.php");
+        exit;
+    }
+?>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>お問い合わせフォーム</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div><h1>Kamekame diving shop</h1></div>
+<div>
+    <form action="confirm.php" method="post">
+            <input type="hidden" name="name" value="<?php echo $name; ?>">
+            <input type="hidden" name="furigana" value="<?php echo $furigana; ?>">
+            <input type="hidden" name="email" value="<?php echo $email; ?>">
+            <input type="hidden" name="tel" value="<?php echo $tel; ?>">
+            <input type="hidden" name="sex" value="<?php echo $sex; ?>">
+            <input type="hidden" name="item" value="<?php echo $item; ?>">
+            <input type="hidden" name="content" value="<?php echo $content; ?>">
+            <h1 class="contact-title">Is there a mistake in your reservation?</h1>
+            <div>
+                <div>
+                    <label>Name</label>
+                    <p><?php echo $name; ?></p>
+                </div>
+                <div>
+                    <label>Email</label>
+                    <p><?php echo $email; ?></p>
+                </div>
+                <div>
+                    <label>Tel</label>
+                    <p><?php echo $tel; ?></p>
+                </div>
+                <div>
+                    <label>Gender</label>
+                    <p><?php echo $sex ?></p>
+                </div>
+                <div>
+                    <label>Course</label>
+                    <p><?php echo $item; ?></p>
+                </div>
+            </div>
+        <input type="button" value="Revising" onclick="history.back(-1)">
+        <button type="submit" name="submit">Reserving</button>
+    </form>
+</div>
+</body>
+</html>
